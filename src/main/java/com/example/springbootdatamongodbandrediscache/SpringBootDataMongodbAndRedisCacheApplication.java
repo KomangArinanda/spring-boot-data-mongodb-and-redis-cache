45be34378d8d46d6a650536cb6b92dbea13a79d0
Komang Arinanda <komang.arinanda@gdn-commerce.com>
2021-09-20 21:03:30 +0700

    [Step 1] Create Project From https://start.spring.io/ or Intellij IDEA then Start
    Dependency:
    Spring Web
    Spring Data MongoDB
    Lombok
    Embedded Mongo Database - Testing

---
+++ src/main/java/com/example/springbootdatamongodbandrediscache/SpringBootDataMongodbAndRedisCacheApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootdatamongodbandrediscache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataMongodbAndRedisCacheApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootDataMongodbAndRedisCacheApplication.class, args);
+  }
+}
---
+++ src/test/java/com/example/springbootdatamongodbandrediscache/SpringBootDataMongodbAndRedisCacheApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.springbootdatamongodbandrediscache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataMongodbAndRedisCacheApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
